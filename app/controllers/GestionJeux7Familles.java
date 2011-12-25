e582803ad8bd83766385b58d202ab387f257bfb8
Frederic Chiron <himurafred@gmail.com>
2011-12-25 23:19:10 +0100

    Ajout de la regle (dev) pour L'Acces-Control

--- app/controllers/GestionJeux7Familles.java
+++ app/controllers/GestionJeux7Familles.java
@@ -4,20 +4,22 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import play.Play;
 import play.mvc.Before;
 import play.mvc.Controller;
 import play.mvc.Http;
 
 public class GestionJeux7Familles extends Controller {
-
 	
 	@Before 
 	public static void setCORS() { 
-		Http.Header hd = new Http.Header(); 
-		hd.name = "Access-Control-Allow-Origin"; 
-		hd.values = new ArrayList<String>(); 
-		hd.values.add("*"); 
-		Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);		
+		if(Play.mode.isDev()){
+			Http.Header hd = new Http.Header(); 
+			hd.name = "Access-Control-Allow-Origin"; 
+			hd.values = new ArrayList<String>(); 
+			hd.values.add("*"); 
+			Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);		
+		}
 	}
 	
     public static void recuperationCartes() {

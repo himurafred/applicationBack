60bbbc004e40153980441bed398777718e44a7c3
Frederic Chiron <himurafred@gmail.com>
2011-12-26 01:01:15 +0100

    Etude du système de properties + petite modification + exemple

--- app/controllers/ControllerParent.java
+++ app/controllers/ControllerParent.java
@@ -1,16 +1,27 @@
 package controllers;
 
 import play.*;
+import play.db.Evolutions;
+import play.jobs.JobsPlugin;
+import play.libs.IO;
 import play.mvc.*;
+import play.utils.OrderSafeProperties;
+import play.vfs.VirtualFile;
 
+import java.io.IOException;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import models.*;
 
 public class ControllerParent extends Controller {
-
+	
+	//Ce champs sert seulement à avoir une meilleure compréhension lors de l'accès aux properties.
+	public static Properties properties = Play.configuration;
+	
 	@Before 
-	public static void setCORS() { 
+	public static void setCORS() { 		
 		if(Play.mode.isDev()){
 			Http.Header hd = new Http.Header(); 
 			hd.name = "Access-Control-Allow-Origin"; 
@@ -18,6 +29,5 @@ public class ControllerParent extends Controller {
 			hd.values.add("*"); 
 			Http.Response.current().headers.put("Access-Control-Allow-Origin",hd);		
 		}
-	}
-
+	}	
 }
--- app/controllers/GestionJeux7Familles.java
+++ app/controllers/GestionJeux7Familles.java
@@ -1,20 +1,27 @@
 package controllers;
 
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
-
-import play.Play;
-import play.mvc.Before;
-import play.mvc.Controller;
-import play.mvc.Http;
 
 public class GestionJeux7Familles extends ControllerParent {
 		
     public static void recuperationCartes() {
+    	
+    	List<String> listeFamilles = Arrays.asList(ControllerParent.properties.getProperty("listeFamilles").split(";"));
+    	List<String> elementsFamille = Arrays.asList(ControllerParent.properties.getProperty("elementsFamille").split(";"));
+    	
+    	//Récupération de la liste des familles
+    	//Exemple utilisation properties    	
+    	/*Map<String, List<String>> values = new HashMap<String, List<String>>();
+    	values.put("listeFamille", listeFamilles);
+    	values.put("elementsFamille", elementsFamille);
+        renderJSON(values);*/
+    
     	Map<String, String> values = new HashMap<String, String>();
-    	values.put("msg", "récupération des cartes done !");
-        renderJSON(values);
+    	values.put("msg", "Les cartes sont prêtes !");
+    	renderJSON(values);
     }
 
 }
